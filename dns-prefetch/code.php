b721138adc7f97c8c921e1940210cdb6a66ad2fe
Andrew Killen <andy@weareswoop.com>
2014-04-30 09:51:41 +0200

    adds check for local script HOST value

--- dns-prefetch/code.php
+++ dns-prefetch/code.php
@@ -18,8 +18,8 @@ function add_dns_prefetch(){
     $base = parse_url(get_bloginfo("url"));    
     foreach($checking as $places){
         foreach($GLOBALS[$places]->queue as $item){
-            $url = parse_url($GLOBALS[$places]->registered[$item]->src);        
-            if(!in_array( "//".$url['host'], $prefetch_array) && $base['host'] != $url['host'] ){
+            $url = parse_url($GLOBALS[$places]->registered[$item]->src);              
+            if(isset($url['host']) && !in_array( "//".$url['host'], $prefetch_array) && $base['host'] != $url['host'] ){
                 $prefetch_array[] = "//".$url['host'];
                 echo "<link rel='dns-prefetch' href='//".$url['host']."'>\r\n";                
             }                
